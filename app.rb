f66cae78fa5ab60fdd395f3237a8bbcfd6d6fe41
Victoria Hale <vic.hale74@gmail.com>
2019-11-19 21:52:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   end
   
   post '/puppy' do 
-    @puppy = Puppy.new(params[:name],[:breed], [:age])
+    @puppy = Puppy.new(params[:name],params[:breed], params[:age])
     erb :display_puppy
   end
 end
