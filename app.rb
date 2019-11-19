16c258217784788de3c3dfa57c89793ec878e9a9
Victoria Hale <vic.hale74@gmail.com>
2019-11-19 21:52:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,8 +5,12 @@ class App < Sinatra::Base
     erb :index
   end
   
+  get '/new' do 
+    erb :create_puppy
+  end
+  
   post '/puppy' do 
-    @puppy = Puppy.new(params[])
+    @puppy = Puppy.new(params[:name],[:breed], [:age])
     erb :display_puppy
   end
 end
